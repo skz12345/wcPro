ec3f88381b8e5f223a082ae45a86af82285b18e8
skz12345 <2019730393@qq.com>
2018-04-08 19:57:25 +0800

    change of Mason‘s input
    change the input code so that it can be good

--- input/input.java
+++ input/input.java
@@ -1,14 +1,16 @@
-package WordTest;
+//author：Mason
+package wordtest;
 import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern; 
+
 
 public class input {
 	public input(){
 		
 	}
-      private String filepath="";//文件路径
-	  private String result="";//读出的结果
+      private String filepath="";//the path of file
+	  private String result="";//the result of input
 	public String getFilepath() {
 		return filepath;
 	}
@@ -22,7 +24,7 @@ public class input {
     	  this.filepath=filepath;
       }
 	
-	public boolean isContainChinese(String str) {//判断是否有中文字符
+	public boolean isContainChinese(String str) {//judgement of Chinese character
 		 Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
 		 Matcher m = p.matcher(str);
 		 if (m.find()) {
@@ -31,26 +33,26 @@ public class input {
 		 return false;
 		}
 	
-     public String openFileAndPull ()//处理输入的核心
+     public String openFileAndPull ()//core part
       {
-    	  if((this.filepath).length()==0)//判断输入是否为空，是则抛出异常
-    		  throw new IllegalArgumentException("Please input the filename!");
+    	  if((this.filepath).length()==0)//judgement of null and then throw exceptions
+    	  { throw new IllegalArgumentException("Please input the filename!");}
     	  File file = new File(filepath);  
-    	  if(!file.getName().endsWith(".txt"))//判断是否文件后缀为.txt，否则抛出异常
-    		  throw new IllegalArgumentException("Error file format!");
+    	  if(!file.getName().endsWith(".txt"))//judgement of “.txt” and then throw exceptions
+    		  {throw new IllegalArgumentException("Error file format!");}
     	  try{
     		  BufferedReader reader=null;
     		  StringBuilder strings=new StringBuilder();
   			  reader=new BufferedReader(new FileReader(file));
   			  String readline="";
-  			while((readline=reader.readLine())!=null)//按行读取文件内容
+  			while((readline=reader.readLine())!=null)//read the file line by line 
   			{
-  				strings.append(readline+"\n");//每行末尾添加一个换行符
+  				strings.append(readline+"\n");//add a \n at the end of each line read out 
   				readline=null;
     	    }
-  			this.result=strings.toString();//将结果存入result
+  			this.result=strings.toString();//result assignment
   			
-  			if(isContainChinese(result))//调用方法判断result中是否有中文字符
+  			if(isContainChinese(result))//judgement of Chinese charactor
   			{
   				System.out.println("（文本中有中文字符）");
   			}
